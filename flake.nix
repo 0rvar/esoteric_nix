4c94bf4043564cd1ce40e8cfcdee8736d5fdd488
Orvar Segerström <orvarsegerstrom@gmail.com>
2024-03-06 11:02:33 +0100

    Properly struture flake output

--- flake.nix
+++ flake.nix
@@ -4,26 +4,31 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-
-  flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils }:
     let
       overlay = import ./overlay.nix;
-
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ overlay ];
-      };
-      overlayPackageNames = builtins.attrNames (overlay {} {});
-      devshellPackages = map (name: pkgs.${name}) overlayPackageNames;
-
-    in {
+    in
+    {
       overlays.default = overlay;
-
-      # Define a devShell for the given system
-      devShells.default = pkgs.mkShell {
-        buildInputs = devshellPackages;
-      };
     }
-  );
+    //
+
+    # Dynamically generate devshell for each system
+    # containing all the packages in the overlay
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
+        overlayPackageNames = builtins.attrNames (overlay { } { });
+        devshellPackages = map (name: pkgs.${name}) overlayPackageNames;
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = devshellPackages;
+        };
+      }
+    );
 }
