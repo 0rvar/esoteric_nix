8d45144012fc7ce6c905f40cd4a32a19416ac911
Orvar Segerström <orvarsegerstrom@gmail.com>
2024-03-06 11:35:27 +0100

    Get rid of flake-utils

--- flake.nix
+++ flake.nix
@@ -1,35 +1,42 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs }:
     let
       overlay = import ./overlay.nix;
+
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ]
+          (system:
+            function (import nixpkgs {
+              inherit system;
+              overlays = [ overlay ];
+            }));
+
     in
     {
       overlays.default = overlay;
-    }
-    //
 
-    # Dynamically generate a shell for each system
-    # containing all the packages in the overlay
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ overlay ];
-        };
-        overlayPackageNames = builtins.attrNames (overlay { } { });
-        overlayPackages = map (name: pkgs.${name}) overlayPackageNames;
-
-      in
-      {
-        packages.default = pkgs.buildEnv {
-          name = "esoteric shell";
-          paths = overlayPackages;
-        };
-      }
-    );
+      # Dynamically generate a shell for each system
+      # containing all the packages in the overlay
+      packages = forAllSystems
+        (pkgs: {
+          default =
+            let
+              overlayPackageNames = builtins.attrNames (overlay { } { });
+              overlayPackages = map (name: pkgs.${name}) overlayPackageNames;
+            in
+            pkgs.buildEnv {
+              name = "esoteric shell";
+              paths = overlayPackages;
+            };
+        });
+    };
 }
